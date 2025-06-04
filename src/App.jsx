3abb803a088e9c19b8052e5112a9cb6e317ca893
Raina Bethea <rainabethea@gmail.com>
2025-06-04 12:21:40 -0400

    setup components of App

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import ColorSelect from './components/ColorSelect.jsx';
+import NumberSelect from './components/NumberSelect.jsx';
+import FortuneReveal from'./components/FortuneReveal.jsx';
+
 
 function App() {
   const [count, setCount] = useState(0)
+  const [color, setColor] = useState(null)
+  const [round1, setRound1] = useState(null)
+  const [round2, setRound2] = useState(null)
+  const [fortune,setFortune] = useState('')
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const handleRestart = () => {
+      setCount(1)
+      setColor(null)
+      setFortune('')
+      setRound1(null)
+      setRound2(null)
+
+  }
 }
 
 export default App
